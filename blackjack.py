0d5ea1df403be001f1dac6f7c49c0d36447569fe
KajoRudzinski <kajo.rudzinski@gmail.com>
2020-06-11 12:01:00 +0200

    Added buttons (without funcion yet) and helpful comments to improve readability

--- blackjack.py
+++ blackjack.py
@@ -19,6 +19,7 @@ card_frame = tk.Frame(
     main_window, relief="sunken", borderwidth=1, bg="green")
 card_frame.grid(row=1, column=0, sticky="ew", columnspan=3, rowspan=2)
 
+# dealer
 dealer_score_label = tk.IntVar()
 tk.Label(
     card_frame, text="Dealer", bg="green", fg="white")\
@@ -27,9 +28,11 @@ tk.Label(
     card_frame, textvariable=dealer_score_label, bg="green", fg="white")\
     .grid(row=1, column=0)
 
+# dealer cards
 dealer_card_frame = tk.Frame(card_frame, bg="green")
 dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
+# player
 player_score_label = tk.IntVar()
 tk.Label(
     card_frame, text="Player", bg="green", fg="white") \
@@ -38,12 +41,20 @@ tk.Label(
     card_frame, textvariable=player_score_label, bg="green", fg="white") \
     .grid(row=3, column=0)
 
+# player cards
+player_card_frame = tk.Frame(card_frame, bg="green")
+player_card_frame.grid(row=2, column=1, sticky="ew", rowspan=2)
 
+# button frames
+button_frame = tk.Frame(main_window)
+button_frame.grid(row=3, column=0, columnspan=3, sticky="w")
 
+# button dealer
+dealer_button = tk.Button(button_frame, text="Dealer")
+dealer_button.grid(row=0, column=0)
 
-
-
-
-
+# button player
+player_button = tk.Button(button_frame, text="Player")
+player_button.grid(row=0, column=1)
 
 main_window.mainloop()
